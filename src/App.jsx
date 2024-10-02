23bbf8822038f7d840e2cce431d205f6930739b5
hihassan1998 <hassanihussain1998@gmail.com>
2024-10-02 03:56:55 +0200

    created navbar and added responsize design requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Navbar from "./components/navbar/Navbar"
+import "./layout.scss"
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="content">
+        <homePage />
+      </div>
+    </div>
   )
 }
 
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,14 @@
+import "./homePage.scss"
+
+function homePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">text</div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default homePage
