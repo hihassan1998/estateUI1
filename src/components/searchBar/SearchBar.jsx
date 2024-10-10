f8bf3b0747b8896316b273125052232651bcd968
hihassan1998 <hassanihussain1998@gmail.com>
2024-10-10 22:54:03 +0200

    added activity and responsicness to the buttons on the searchbar, added styling to the homepage

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./searchBar.scss"
 
+const types = ["buy", "rent"];
 function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }))
+  };
+
   return (
     <div className='searchBar'>
-        <div className='type'></div>
-        <form type="text">
-            <input type="text" nae="location" placeholder='City Location' />
-            <input type="number" nae="minPrice" placeholder='Min Price' />
-            <input type="number" nae="maxPrice" placeholder='Max Price' />
-        </form>
-      
-    </div>
+      <div className='type'>
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+
+      </div>
+      <form>
+        <input
+          type="text"
+          name="location"
+          placeholder='City Location'
+        />
+        <input type="number"
+          name="minPrice"
+          placeholder='Min Price'
+        />
+        <input type="number"
+          name="maxPrice"
+          placeholder='Max Price'
+        />
+        <button>
+          <img src="/search.png" alt="Search" />
+        </button>
+      </form>
+
+    </div >
   )
 }
 
